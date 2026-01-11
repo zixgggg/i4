8a8fd8e2b8370be1dda40fd48356e880060b2db0
zixgggg <zixzix035@gmail.com>
2026-01-11 22:19:37 +0800

    kill window function still not finish

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from Xlib import X, XK
 dpy = Display()
 screen = dpy.screen()
 root = screen.root
-root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("m")),X.Mod4Mask,1,X.GrabModeAsync,X.GrabModeAsync)
+root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("z")),X.Mod4Mask,1,X.GrabModeAsync,X.GrabModeAsync)
+root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("q")),X.Mod4Mask,X.ShiftMask,1,X.GrabModeAsync,X.GrabModeAsync)
+
 def call_rofi():
     if ev.type==X.KeyPress:
         #print(f"press key:{ev.detail}")
-        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("m")):
+        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("z")):
             subprocess.Popen(["rofi","-show","drun"])
+def kill_window():
+    if ev.type==X.KeyPress:
+        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("q")):
+            window.kill_client()
 while True:
     ev=dpy.next_event()
     call_rofi()
+    kill_window()
     dpy.flush()
