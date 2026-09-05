4b4e207da6f4059acaeff45c1d3db9ad798ccaef
zixgggg <zixzix035@gmail.com>
2026-09-05 21:30:17 +0800

    add file

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-import subprocess
-from Xlib.display import Display
-from Xlib import X, XK
-dpy = Display()
-screen = dpy.screen()
-root = screen.root
-root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("z")),X.Mod4Mask|X.Mod2Mask,1,X.GrabModeAsync,X.GrabModeAsync)
-root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("q")),X.Mod4Mask|X.Mod2Mask,X.ShiftMask,1,X.GrabModeAsync,X.GrabModeAsync)
-
-def call_rofi():
-    if ev.type==X.KeyPress:
-        #print(f"press key:{ev.detail}")
-        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("z")):
-            subprocess.Popen(["rofi","-show","drun"])
-def kill_window():
-    if ev.type==X.KeyPress:
-        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("q")):
-            window.kill_client()
+from Xlib import display,X,XK
+dpy=display.Display()
+screen=dpy.screen()
+root=screen.root
+#註冊事件
+screen.root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("q")),X.Mod4Mask,1,X.GrabModeAsync, X.GrabModeAsync)
+#cursor=root.create_font_cursor(0)
+#cursor=dpy.create_resource_object("cursor")
+#root.define_cursor(cursor)
 while True:
-    ev=dpy.next_event()
-    call_rofi()
-    kill_window()
+    event=dpy.next_event()
+    if event.type==X.KeyPress:
+        if event.detail==dpy.keysym_to_keycode(XK.string_to_keysym("q")):
+            X.kill_client(dpy)
     dpy.flush()
---
+++ old_main.py
@@ -0,0 +1,23 @@
+import subprocess
+from Xlib.display import Display
+from Xlib import X, XK
+dpy = Display()
+screen = dpy.screen()
+root = screen.root
+root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("z")),X.Mod4Mask|X.Mod2Mask,1,X.GrabModeAsync,X.GrabModeAsync)
+root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("q")),X.Mod4Mask|X.Mod2Mask,X.ShiftMask,1,X.GrabModeAsync,X.GrabModeAsync)
+
+def call_rofi():
+    if ev.type==X.KeyPress:
+        #print(f"press key:{ev.detail}")
+        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("z")):
+            subprocess.Popen(["rofi","-show","drun"])
+def kill_window():
+    if ev.type==X.KeyPress:
+        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("q")):
+            window.kill_client()
+while True:
+    ev=dpy.next_event()
+    call_rofi()
+    kill_window()
+    dpy.flush()
